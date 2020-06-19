92bc056587ec6bffd3561b0e18443d6e36a993db
wanhongxu <954335800@qq.com>
2020-06-19 17:51:15 +0800

    第二次提交

--- product-view-service-feign/src/main/java/cn/how2j/springcloud/util/FreshConfigUtil.java
+++ product-view-service-feign/src/main/java/cn/how2j/springcloud/util/FreshConfigUtil.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 /**
  * Created by wanhongxu on 2020/3/31.
  */
+
 public class FreshConfigUtil {
     public static void main(String[] args) {
         HashMap<String,String> headers =new HashMap<>();
