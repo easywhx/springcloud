d9c86ce42cbe2350339ea0979cb6e7f22f808b6e
wanhongxu <954335800@qq.com>
2020-06-23 12:30:27 +0800

    添加了lombok  接口文档swagger  日志配置注解@slf4j

---
+++ admin-server/src/main/java/cn/how2j/springcloud/AdminServerApplication.java
@@ -0,0 +1,16 @@
+package cn.how2j.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: AdminServerApplication <br/>
+ * Function: TODO. <br/>
+ * Date: 2020/6/23 12:09 <br/>
+ *
+ * @author wanhongxu
+ * @version 1.0
+ * @since JDK 1.7
+ */
+@SpringBootApplication
+public class AdminServerApplication {
+}
