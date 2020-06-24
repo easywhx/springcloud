90294c9b9507874d36a0cce1390bc8524f203495
wanhongxu <954335800@qq.com>
2020-06-24 09:26:20 +0800

    Swagger汇总api接口文档

--- productDataservice/src/main/java/cn/how2j/springcloud/ProductDataServiceApplication.java
+++ productDataservice/src/main/java/cn/how2j/springcloud/ProductDataServiceApplication.java
@@ -10,8 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -25,6 +32,7 @@ import java.util.concurrent.TimeoutException;
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("cn.how2j.springcloud.mapper")
+@EnableSwagger2
 public class ProductDataServiceApplication {
     public static void main(String[] args) {
         int port = 0;
@@ -60,6 +68,26 @@ public class ProductDataServiceApplication {
         }
 
         new SpringApplicationBuilder(ProductDataServiceApplication.class).properties("server.port=" + port).run(args);
+    }
+
+    @Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+        @Override
+        public List<SwaggerResource> get() {
+            List resources = new ArrayList<>();
+            resources.add(swaggerResource("service-a", "/swagger-service-a/v2/api-docs", "2.0"));
+            resources.add(swaggerResource("service-b", "/swagger-service-b/v2/api-docs", "2.0"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
     }
 
     @Bean
